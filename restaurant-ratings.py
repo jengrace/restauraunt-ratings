5a0e326da73b477a7487b2cd5284df1fd2f19547
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 18:02:56 -0800

    Interactive version with user

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,28 +1,48 @@
 # your code goes here
 
-
-def get_sorted_resturaunts(filename):
-    """ Sorting resturaunts alphabetically and creating a dictionary
-    of restaurants with their corresponding score.
-    """
-    
+def convert_file_to_dict(filename):
     file_ref = open(filename)
     rest_ratings = {}
 
-    for line in file_ref:
-        
+    for line in file_ref:     
         rest_name, rest_score = line.rstrip().split(':')
-
         rest_ratings[rest_name] = rest_score
-    
 
     file_ref.close()
 
-
-    for restaurant, score in sorted(rest_ratings.items()):
-        print restaurant + ' is rated at ' + score + '.'
+    return rest_ratings
 
 
+def get_sort_dictionary(dictionary):
+    """ Sorting resturaunts alphabetically and creating a dictionary
+    of restaurants with their corresponding score.
+    """
+#    rest_ratings = convert_file_to_dict('scores.txt')
+    #try:    
+    for name, score in sorted(dictionary.items()):
+        print name + ' is rated at ' + str(score) + '.'
+    #except:
+        #"A mistake somewhere..."
 
-get_sorted_resturaunts('scores.txt')
+existing_dict = convert_file_to_dict('scores.txt') 
 
+
+custom_rest = raw_input("Please choose a restaurant: ").title()
+
+
+if custom_rest in existing_dict:
+    custom_ans = raw_input("This restaurant is already rated. Would you like to change the score? Y/N: ").upper()
+    
+    if custom_ans == 'Y':
+        custom_score = int(raw_input("Please score chosen restaurant: "))
+       
+    
+    elif custom_ans == 'N':
+        custom_rest = raw_input("Please choose a restaurant: ").title()
+        custom_score = int(raw_input("Please score chosen restaurant: "))
+
+else:
+     custom_score = int(raw_input("Please score chosen restaurant: "))
+
+existing_dict[custom_rest] = custom_score
+get_sort_dictionary(existing_dict)
